059e05bb1eace35190460e52f0bc273042573ac3
kingsleyusoroeno <kingsleyenou@gmail.com>
2019-02-08 10:49:41 +0100

    implemented pagination in the RecyclerView

--- app/src/main/java/techgroup/com/retrofittutorial/MainActivity.java
+++ app/src/main/java/techgroup/com/retrofittutorial/MainActivity.java
@@ -1,11 +1,11 @@
 package techgroup.com.retrofittutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,39 +22,60 @@ public class MainActivity extends AppCompatActivity {
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
     RecyclerView mRecyclerView;
     PostRecyclerViewAdapter postRecyclerViewAdapter;
-    List<PostAttributes> postAttributes;
+    List<PostAttributes> postArray;
     private static final String TAG = "MainActivity";
+    Handler handler;
+    private Retrofit retrofit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
-        postAttributes = new ArrayList<>();
+        mRecyclerView = findViewById(R.id.RecyclerView);
+        handler = new Handler();
 
-        // Tv = (TextView) findViewById(R.id.TextView);
-        postRecyclerViewAdapter = new PostRecyclerViewAdapter(MainActivity.this);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setAdapter(postRecyclerViewAdapter);
-
-
-        Retrofit retrofit = new Retrofit.Builder()
+        // Retrofit instantiation
+        retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+        getPostArray();
 
+        // Tv = (TextView) findViewById(R.id.TextView);
+        postArray = new ArrayList<>();
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        postRecyclerViewAdapter = new PostRecyclerViewAdapter(MainActivity.this, mRecyclerView);
+        postRecyclerViewAdapter.setPostList(postArray);
+        Log.d(TAG, "setPostList: " + postArray);
+        mRecyclerView.setAdapter(postRecyclerViewAdapter);
+        postRecyclerViewAdapter.setOnLoadMoreListener(new PostRecyclerViewAdapter.OnLoadMoreListener() {
+            @Override
+            public void onLoadMore() {
+                postArray.add(null);
+                postRecyclerViewAdapter.notifyItemInserted(postArray.size() - 1);
+                postArray.remove(postArray.size() - 1);
+                postRecyclerViewAdapter.notifyItemRemoved(postArray.size());
+                for (int i = 0; i < 15; i++) {
+                    postArray.add(new PostAttributes(3, 2, "Just Added", "To my RecyclerView"));
+                    postRecyclerViewAdapter.notifyItemInserted(postArray.size());
+                }
+                postRecyclerViewAdapter.setLoaded();
+                Log.d(TAG, "onLoadMore: Loading");
+            }
+        });
+    }
+
+    private void getPostArray() {
         JsonPlaceHolderApi placeHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-
-        Call<List<PostAttributes>> post = placeHolderApi.getAllPost();
+        final Call<List<PostAttributes>> post = placeHolderApi.getAllPost();
 
         post.enqueue(new Callback<List<PostAttributes>>() {
             @Override
             public void onResponse(Call<List<PostAttributes>> call, Response<List<PostAttributes>> response) {
-
-                    postAttributes = response.body();
-                    postRecyclerViewAdapter.setPostList(postAttributes);
-                    
+                postArray = response.body();
+                postRecyclerViewAdapter.setPostList(postArray);
+                Log.d(TAG, "postArray Size: " + postArray.size());
             }
 
             @Override
@@ -63,5 +84,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
--- app/src/main/java/techgroup/com/retrofittutorial/PostRecyclerViewAdapter.java
+++ app/src/main/java/techgroup/com/retrofittutorial/PostRecyclerViewAdapter.java
@@ -2,6 +2,7 @@ package techgroup.com.retrofittutorial;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +15,30 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
 
    private List<PostAttributes> postList;
    private Context mContext;
+    private int visibleThreshold = 5;
+    private int lastVisibleItem, totalItemCount;
+    private boolean loading;
+    private OnLoadMoreListener onLoadMoreListener;
 
-    public PostRecyclerViewAdapter(Context context){
+    public PostRecyclerViewAdapter(Context context, RecyclerView recyclerView){
         this.mContext = context;
+        if (recyclerView.getLayoutManager() instanceof LinearLayoutManager){
+            final LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+            recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    totalItemCount = layoutManager.getItemCount();
+                    lastVisibleItem = layoutManager.findLastVisibleItemPosition();
+                    if (!loading && totalItemCount <= (lastVisibleItem + visibleThreshold)){
+                        if (onLoadMoreListener != null){
+                            onLoadMoreListener.onLoadMore();
+                        }
+                        loading = true;
+                    }
+                }
+            });
+        }
     }
 
     @NonNull
@@ -28,13 +50,11 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
 
     @Override
     public void onBindViewHolder(@NonNull PostViewHolder holder, int position) {
-
         PostAttributes  post = postList.get(holder.getAdapterPosition());
         holder.userId.setText(String.valueOf(post.getUserId()));
         holder.Id.setText(String.valueOf(post.getId()));
         holder.title.setText(post.getTitle());
         holder.body.setText(post.getBody());
-
     }
 
     @Override
@@ -44,9 +64,22 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
         }else
         return postList.size();
     }
+
     public void setPostList(List<PostAttributes> postList){
         this.postList = postList;
         notifyDataSetChanged();
+    }
+
+    public interface OnLoadMoreListener {
+        void onLoadMore();
+    }
+
+    public void setLoaded() {
+        loading = false;
+    }
+
+    public void setOnLoadMoreListener(OnLoadMoreListener onLoadMoreListener){
+        this.onLoadMoreListener = onLoadMoreListener;
     }
 
     public class PostViewHolder extends RecyclerView.ViewHolder {
@@ -57,10 +90,10 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
 
         public PostViewHolder(View itemView) {
             super(itemView);
-            userId = (TextView) itemView.findViewById(R.id.TextView_UserId);
-            Id = (TextView) itemView.findViewById(R.id.TextView_id);
-            title = (TextView) itemView.findViewById(R.id.TextView_Title);
-            body = (TextView) itemView.findViewById(R.id.TextView_Body);
+            userId = itemView.findViewById(R.id.TextView_UserId);
+            Id = itemView.findViewById(R.id.TextView_id);
+            title = itemView.findViewById(R.id.TextView_Title);
+            body = itemView.findViewById(R.id.TextView_Body);
         }
     }
 }
